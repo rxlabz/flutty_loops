afdb99f27d9c31a6772ce05326a5ddd9791c45b0
Erick Ghaumez <rxlabz@gmail.com>
2019-05-22 09:14:22 +0200

    unminify step1

--- lib/main.dart
+++ lib/main.dart
@@ -6,85 +6,137 @@ import 'package:flutter/widgets.dart';
 import 'package:quiver/async.dart' as q;
 import 'package:quiver/iterables.dart';
 
+/// a millisecond
 const ms = Duration(milliseconds: 1);
-const gr = Color(0xaa404040);
-const grd = Color(0xcc1a1a1a);
-const yLow = Colors.yellow;
-const bip = const MethodChannel('bz.rxla/tone');
 
-snd(Iterable<int> ids) => bip.invokeMethod('play', ids.toList());
+// grey color
+const grey = Color(0xaa404040);
 
-Paint pnt(Color c) => Paint()..color = c;
+// dark grey color
+const darkGrey = Color(0xcc1a1a1a);
 
-tpo(f) => 60 ~/ (f / 1000);
+// yellow color
+const yellow = Colors.yellow;
+
+/// sound MethodChannel : permet d'envoyer les "instructions de génération sonore"
+/// à la platform ( ios ou android )
+const soundChannel = const MethodChannel('bz.rxla/tone');
+
+/// envoie une liste d'instruction
+playSounds(Iterable<int> ids) =>
+    soundChannel.invokeMethod('play', ids.toList());
+
+/// génére un painter coloré
+Paint paintWith(Color c) => Paint()..color = c;
+
+/// calcul le tempo correspondant à un interval en mms
+tempoFor(int interval) => 60 ~/ (interval / 1000);
 
 void main() => runApp(MaterialApp(
     theme: ThemeData.dark(),
     home: SafeArea(
         child: Scaffold(
-            backgroundColor: grd, body: SizedBox.expand(child: App())))));
+            backgroundColor: darkGrey, body: SizedBox.expand(child: App())))));
 
 class App extends StatefulWidget {
   @override
-  Stt createState() => Stt();
+  AppState createState() => AppState();
 }
 
-class Stt extends State<App> {
-  int id = -1, fq = 666;
-  List<bool> ks = List.filled(8 * 6, false)..setRange(8, 9, [true]);
-  StreamController<int> st = StreamController.broadcast();
-  StreamSubscription sub;
-  OverlayEntry ovrl;
-  bool introOn = true;
-  get _on => sub != null && !sub.isPaused;
+class AppState extends State<App> {
+  /// index de la colonne active
+  int currentColumnIndex = -1;
+
+  /// interval de déplacement de "la tête de lecture"
+  int interval = 666;
+
+  /// tableau des cases, une case rempli au départ pour l'exemple
+  List<bool> cells = List.filled(8 * 6, false)..setRange(8, 9, [true]);
+
+  /// ? streamer principal
+  StreamController<int> streamer = StreamController.broadcast();
+
+  /// souscription au strem
+  StreamSubscription streamSubscription;
+
+  /// ? overlay d'affichage des instructions et du tempo
+  OverlayEntry overlay;
+
+  /// flag d'affichage des instructions au lancement
+  bool showInstruction = true;
+
+  /// en cours de lecture ?
+  get _isRunning => streamSubscription != null && !streamSubscription.isPaused;
+
+  /// renderObject de l'app
   get rbx => context.findRenderObject() as RenderBox;
-  get gl => rbx.globalToLocal;
-  get mqh => rbx.size.height;
-  up(f) => setState(f);
+
+  /// methode de "conversion position"
+  get ToLocalPosition => rbx.globalToLocal;
+
+  /// hauteur du context
+  get contextHeight => rbx.size.height;
+
+  /// applique un changement d'état FIXME rm
+  updateState(f) => setState(f);
+
   gp(o) => o.globalPosition;
 
   @override
   initState() {
     super.initState();
-    st.stream.listen((t) => up(() => fq = t));
-    st.add(fq);
-    go(fq);
+
+    streamer.stream.listen((t) => updateState(() => interval = t));
+    streamer.add(interval);
+    startAt(interval);
   }
 
-  go(int t) {
-    sub?.cancel();
-    sub = q.Metronome.periodic(ms * t).listen((d) {
-      id = (id + 1) % 48;
-      snd(enumerate(List.generate(6, (i) => ks[(id % 8) + (i * 8)]))
+  /// lance la lecture à partir de l'index transmis
+  startAt(int t) {
+    streamSubscription?.cancel();
+    streamSubscription = q.Metronome.periodic(ms * t).listen((d) {
+      currentColumnIndex = (currentColumnIndex + 1) % 48;
+      playSounds(enumerate(List.generate(
+              6, (i) => cells[(currentColumnIndex % 8) + (i * 8)]))
           .where((v) => v.value)
           .map((v) => v.index + 1));
-      up(() {});
+      updateState(() {});
     });
   }
 
-  togl() => _on ? sub.pause() : go(fq);
-  nwfq(Offset o) => 250 + (gl(o).dy / mqh * 750);
-  tap(int p) => up(() => ks[p] = !ks[p]);
-  int posId(Offset p) =>
-      (p.dx / (rbx.size.width / 8)).floor() + (p.dy / (mqh / 6)).floor() * 8;
+  /// pause le timer
+  playPause() => _isRunning ? streamSubscription.pause() : startAt(interval);
+
+  /// calcul le tempo à partir de la position finale du slider
+  onNewInterval(Offset tempoSliderLastPosition) =>
+      250 + (ToLocalPosition(tempoSliderLastPosition).dy / contextHeight * 750);
+
+  ///
+  onCellTap(int cellIndex) =>
+      updateState(() => cells[cellIndex] = !cells[cellIndex]);
+
+  /// correspondance entre la position du touch et la cellule du tableau
+  int positionToCellIndex(Offset p) =>
+      (p.dx / (rbx.size.width / 8)).floor() +
+      (p.dy / (contextHeight / 6)).floor() * 8;
 
   @override
   build(BuildContext _) => GestureDetector(
-      onDoubleTap: togl,
-      onVerticalDragStart: (o) => ovr(gl(gp(o))),
-      onVerticalDragUpdate: (o) => st.add(nwfq(gp(o)).toInt()),
+      onDoubleTap: playPause,
+      onVerticalDragStart: (o) => showTempoOverlay(ToLocalPosition(gp(o))),
+      onVerticalDragUpdate: (o) => streamer.add(onNewInterval(gp(o)).toInt()),
       onVerticalDragEnd: (o) {
-        ovrl?.remove();
-        up(() => go(fq));
+        overlay?.remove();
+        updateState(() => startAt(interval));
       },
-      onTapDown: (t) => tap(posId(gl(gp(t)))),
+      onTapDown: (t) => onCellTap(positionToCellIndex(ToLocalPosition(gp(t)))),
       child: Stack(children: [
-        Sq(ks, id % 8, fq),
+        Sq(cells, currentColumnIndex % 8, interval),
         InkWell(
-          onTap: () => up(() => introOn = false),
-          child: introOn
+          onTap: () => updateState(() => showInstruction = false),
+          child: showInstruction
               ? Container(
-                  color: grd,
+                  color: darkGrey,
                   constraints: BoxConstraints.expand(),
                   child: Center(
                       child: Text(
@@ -93,28 +145,29 @@ class Stt extends State<App> {
                           'Change the tempo : Slide up/down\n'
                           'Turn on speakers & Tap to start',
                           textAlign: TextAlign.center,
-                          style:
-                              TextStyle(color: yLow, fontSize: 20, height: 3))))
+                          style: TextStyle(
+                              color: yellow, fontSize: 20, height: 3))))
               : SizedBox(),
         )
       ]));
 
-  ovr(Offset pos) {
-    ovrl = OverlayEntry(builder: (_) => _bldOvrl(pos));
-    Overlay.of(context).insert(ovrl);
+  /// affiche
+  showTempoOverlay(Offset pos) {
+    overlay = OverlayEntry(builder: (_) => _buildTempoOverlay(pos));
+    Overlay.of(context).insert(overlay);
   }
 
-  _bldOvrl(Offset pos) => Positioned(
+  _buildTempoOverlay(Offset pos) => Positioned(
       left: pos.dx - 30,
       child: SafeArea(
         child: Container(
             width: 60,
-            height: mqh,
-            color: grd,
+            height: contextHeight,
+            color: darkGrey,
             child: StreamBuilder(
-                stream: st.stream,
+                stream: streamer.stream,
                 builder: (_, s) =>
-                    CustomPaint(painter: TPtr(tpo(s.data ?? fq))))),
+                    CustomPaint(painter: TPtr(tempoFor(s.data ?? interval))))),
       ));
 }
 
@@ -125,8 +178,8 @@ class TPtr extends CustomPainter {
   void paint(Canvas cv, Size sz) {
     var sh = sz.height;
     var h = sh * ((t - 60) / 180);
-    cv.drawRect(
-        Rect.fromPoints(Offset(sz.width, sh), Offset(0, sh - h)), pnt(yLow));
+    cv.drawRect(Rect.fromPoints(Offset(sz.width, sh), Offset(0, sh - h)),
+        paintWith(yellow));
   }
 
   @override
@@ -141,7 +194,7 @@ class Sq extends StatelessWidget {
   @override
   Widget build(BuildContext _) => Stack(children: [
         Positioned.fill(child: CustomPaint(painter: Grd(List.from(ks), id))),
-        Positioned(right: 10, top: 10, child: Text('${tpo(fq)}bpm'))
+        Positioned(right: 10, top: 10, child: Text('${tempoFor(fq)}bpm'))
       ]);
 }
 
@@ -160,9 +213,12 @@ class Grd extends CustomPainter {
       final r = Rect.fromPoints(p.value, p.value + Offset(btW - 4, btH - 4));
       final rr = RRect.fromRectAndRadius(r, Radius.circular(8));
       final on = ks[p.index];
-      final color = on ? Color(0xff103335) : grd;
-      cnv.drawRRect(rr,
-          (p.index % 8) == id ? pnt(rbw.withOpacity(on ? 1 : .2)) : pnt(color));
+      final color = on ? Color(0xff103335) : darkGrey;
+      cnv.drawRRect(
+          rr,
+          (p.index % 8) == id
+              ? paintWith(rbw.withOpacity(on ? 1 : .2))
+              : paintWith(color));
     });
   }
 
